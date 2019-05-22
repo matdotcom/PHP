cc161076cbc2e13f09a9196b42244761582f11e4
Mathias Bruun Hemmingsen <31951525+tejsfrank@users.noreply.github.com>
2019-05-22 13:24:32 +0200

    Add files via upload

---
+++ cookie.php
@@ -0,0 +1,11 @@
+<form action="cookie.php" method="POST">
+<input type="text" name="navn" value="hej">
+<input type="submit">
+</form>
+
+<?php
+$navn = $_POST["navn"];
+    echo "<br>\nNavnet er <b>" . $navn . "</b>";
+    setcookie("brugernavn", $navn, time() + (86400 * 30), "/"); // 86400 = 1 day
+    echo "<br/><br/>For at tjekke cookie, skrives dette i console: 'document.cookie'";
+    ?>
